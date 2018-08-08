b3c77e3f08bd41a2da83648ed344ccbfc73c88d9
brwhale <lazorpwnage@gmail.com>
2018-08-07 17:25:10 -0700

    better var names and disable resize

--- FunctionLib/Life.fs
+++ FunctionLib/Life.fs
@@ -2,21 +2,21 @@
 
 module Life =
     // wrap values for edge wrapping
-    let rec wrapClamp i max =
-        match i with
-        | v when v < 0 -> wrapClamp (i+max) max
-        | v when v >= max -> wrapClamp (i-max) max
-        | _ -> i
+    let rec wrapClamp n max =
+        match n with
+        | v when v < 0 -> wrapClamp (n+max) max
+        | v when v >= max -> wrapClamp (n-max) max
+        | _ -> n
 
     // convert bool for indexed list to numeric value
-    let getCellValue i (l :array<bool>) =
-        match l.[(wrapClamp i l.Length)] with
+    let getCellValue index (l :array<bool>) =
+        match l.[(wrapClamp index l.Length)] with
         | true -> 1
         | false -> 0
 
     // count number of living neighbors
-    let getNeighbors i list stride =
-        List.fold (fun count st -> count + (getCellValue (st+i) list)) 0 stride    
+    let getNeighbors index list offsets =
+        List.fold (fun count offset -> count + (getCellValue (offset+index) list)) 0 offsets    
     
     // conway's game of life rules:
     // 3 neighbors always live and 2 always keep their state
